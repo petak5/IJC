d6d7dd91898c9887442c13207309f52835eb6596
Urgoš Peter <peterurgos12345@gmail.com>
2020-03-07 11:14:43 +0100

    Initial structure for ppm module.

---
+++ project1/ppm.h
@@ -0,0 +1,9 @@
+struct ppm {
+	unsigned xsize;
+	unsigned ysize;
+	char data[];    // RGB bajty, celkem 3*xsize*ysize
+};
+
+struct ppm * ppm_read(const char * filename);
+
+void ppm_free(struct ppm *p);
