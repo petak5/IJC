b45bce06f0fbf349c513bea7d22c4dd5c29133a5
petak5 <peterurgos12345@gmail.com>
2020-04-16 22:10:20 +0200

    Implemented half of wordcount functionality.

--- project2/wordcount.c
+++ project2/wordcount.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "htab.h"
 
 #define BUCKETS 1023
@@ -5,5 +7,18 @@
 int main()
 {
     htab_t *htab = htab_init(BUCKETS);
+    if (htab == NULL)
+    {
+        fprintf(stderr, "An error occured.\n");
+        return 1;
+    }
+
+    // todo: implement reading (safe reading!)
+
+    for (htab_iterator_t iterator = htab_begin(htab); !htab_iterator_equal(iterator, htab_end(htab)); iterator = htab_iterator_next(iterator))
+    {
+        printf("%s\t%d\n", htab_iterator_get_key(iterator), htab_iterator_get_value(iterator));
+    }
+
     return htab_hash_fun("Hello, World!");
 }
