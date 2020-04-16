abbca2ba1e3c1580a97265ccebc41de1473642bf
petak5 <peterurgos12345@gmail.com>
2020-04-16 20:32:18 +0200

    Implemented htab_begin.

--- project2/htab_begin.c
+++ project2/htab_begin.c
@@ -2,5 +2,20 @@
 
 htab_iterator_t htab_begin(const htab_t * t)
 {
-    exit(1);
+    htab_iterator_t iterator;
+    iterator.ptr = t->items[0];
+    iterator.t = t;
+    iterator.idx = 0;
+
+    while (!htab_iterator_valid(iterator))
+    {
+        iterator = htab_iterator_next(iterator);
+
+        if (htab_iterator_equal(iterator, htab_end(t)))
+        {
+            break;
+        }
+    }
+
+    return iterator;
 }
