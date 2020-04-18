e0849b8845669280eddc5d3381f53ee5929c70ef
petak5 <peterurgos12345@gmail.com>
2020-04-18 08:46:22 +0200

    Implemented htab_iterator_next.

--- project2/htab_iterator_next.c
+++ project2/htab_iterator_next.c
@@ -2,5 +2,27 @@
 
 htab_iterator_t htab_iterator_next(htab_iterator_t it)
 {
-    exit(1);
+    if (htab_iterator_equal(it, htab_end(it.t)))
+    {
+        return htab_end(it.t);
+    }
+
+    if (it.ptr->next != NULL)
+    {
+        it.ptr = it.ptr->next;
+        return it;
+    }
+
+    while (it.ptr->next == NULL)
+    {
+        it.idx++;
+        it.ptr = it.t->items[it.idx];
+
+        if (it.idx == htab_bucket_count(it.t))
+        {
+            return htab_end(it.t);
+        }
+    }
+
+    return it;
 }
