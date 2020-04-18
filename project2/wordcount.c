7b100300dc90fd0a6257021d9583e8b2668b9163
petak5 <peterurgos12345@gmail.com>
2020-04-18 14:16:43 +0200

    Implemented the rest of wordcount.

--- project2/wordcount.c
+++ project2/wordcount.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
 #include "htab.h"
+#include "io.h"
 
 #define BUCKETS 1023
+#define WORD_LENGTH 127
 
 int main()
 {
     htab_t *htab = htab_init(BUCKETS);
     if (htab == NULL)
     {
-        fprintf(stderr, "An error occured.\n");
+        fprintf(stderr, "Error: failed to allocate memory.\n");
         return 1;
     }
 
-    // todo: implement reading (safe reading!)
+    char word[WORD_LENGTH];
+    while (get_word(word, WORD_LENGTH, stdin) != EOF)
+    {
+        htab_iterator_t iterator =  htab_lookup_add(htab, word);
+        htab_iterator_set_value(iterator, htab_iterator_get_value(iterator) + 1);
+    }
 
     for (htab_iterator_t iterator = htab_begin(htab); !htab_iterator_equal(iterator, htab_end(htab)); iterator = htab_iterator_next(iterator))
     {
         printf("%s\t%d\n", htab_iterator_get_key(iterator), htab_iterator_get_value(iterator));
     }
 
-    return htab_hash_fun("Hello, World!");
+    return 0;
 }
