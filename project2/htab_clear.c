eb11599175402421dbc3345d1f522c687b3ac4dd
petak5 <peterurgos12345@gmail.com>
2020-04-18 12:06:01 +0200

    Implemented htab_clear.

--- project2/htab_clear.c
+++ project2/htab_clear.c
@@ -2,5 +2,27 @@
 
 void htab_clear(htab_t * t)
 {
-    exit(1);
+    for (size_t i = 0; i < htab_bucket_count(t); i++)
+    {
+        if (t->items[i] == NULL)
+        {
+            continue;
+        }
+
+        struct htab_item *item = t->items[i];
+        struct htab_item *temp = NULL;
+
+        while (item->next != NULL)
+        {
+            temp = item->next;
+            
+            free((void *)item->key);
+            free((void *)item);
+
+            item = temp;
+        }
+
+        free((void *)item->key);
+        free((void *)item);
+    }
 }
