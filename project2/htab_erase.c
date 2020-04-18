a1ea33059f51eaa6d3a108fbe8773960548e392c
petak5 <peterurgos12345@gmail.com>
2020-04-18 17:28:35 +0200

    Implemented htab_erase.

--- project2/htab_erase.c
+++ project2/htab_erase.c
@@ -2,5 +2,31 @@
 
 void htab_erase(htab_t * t, htab_iterator_t it)
 {
-    exit(1);
+    htab_iterator_t iterator;
+    iterator.t = it.t;
+    iterator.idx = it.idx;
+    iterator.ptr = iterator.t->items[iterator.idx];
+
+    if (htab_iterator_equal(iterator, it))
+    {
+        struct htab_item *temp = iterator.ptr->next;
+        free((void *)iterator.ptr->key);
+        free((void *)iterator.ptr);
+        t->items[iterator.idx] = temp;
+
+        return;
+    }
+
+    struct htab_item *previous_item = NULL;
+    while (!htab_iterator_equal(iterator, it))
+    {
+        previous_item = iterator.ptr;
+        iterator.ptr = iterator.ptr->next;
+    }
+
+    previous_item->next = iterator.ptr->next;
+    free((void *)iterator.ptr->key);
+    free((void *)iterator.ptr);
+
+    return;
 }
