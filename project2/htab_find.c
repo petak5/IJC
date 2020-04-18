efb97e84faecb4d1fae33b2e5a4a3d9c55f79545
petak5 <peterurgos12345@gmail.com>
2020-04-18 08:53:09 +0200

    Implemented htab_find.

--- project2/htab_find.c
+++ project2/htab_find.c
@@ -2,5 +2,17 @@
 
 htab_iterator_t htab_find(htab_t * t, htab_key_t key)
 {
-    exit(1);
+    htab_iterator_t iterator = htab_begin(t);
+
+    while (!htab_iterator_equal(iterator, htab_end(t)))
+    {
+        if (strcmp(iterator.ptr->key, key) == 0)
+        {
+            return iterator;
+        }
+
+        iterator = htab_iterator_next(iterator);
+    }
+
+    return htab_end(t);
 }
